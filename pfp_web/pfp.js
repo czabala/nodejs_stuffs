94d44f3560a424dd1a263593d9b3dc536c1070a5
Carlo <carlo_zabala@yahoo.com>
2018-05-08 15:16:23 +1200

    Modify PFP basic

--- pfp_web/pfp.js
+++ pfp_web/pfp.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use('/PFP', express.static('./PFP_SAMPLE_FOLDER/8270080642'));
+app.use('/PFP', express.static('./PFP_SAMPLE_FOLDER/8270080642'), (req,res,next) => {
+	console.log(`/PFP requested of type:${req.method}, ${req.originalUrl}`);
+	next();
+});
 
 app.listen(4040, () => console.log('listening to port 4040'));
 
