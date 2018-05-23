58957e131aafc76880cc1d38e63e102a5f1b7015
Carlo <carlo_zabala@yahoo.com>
2018-05-23 15:34:43 +1200

    added latest changes

--- http_apps/http_app_file.js
+++ http_apps/http_app_file.js
@@ -23,6 +23,7 @@ http.createServer((request, response) => {
   
   request.on('end',function(){
 	var post = qs.parse(requestBody);
+	console.log(`Writing to file ./request.txt for request ${request.method} and ${request.url}`);
 	fs.writeFile("./request.txt",requestBody, (err) =>
 	{
 		if (err) throw err;	
@@ -38,9 +39,33 @@ http.createServer((request, response) => {
 
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Hey I\'m okay!!!!');
+  } else if (request.method === 'POST' && request.url === '/request'){
+    response.writeHead(200, { 'Content-Type': 'text/xml' });
+    response.end(`<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:aucklandcouncil.govt.nz:service:supplier:operations:componentmodel:order">
+   <soap:Header/>
+   <soap:Body>
+	<updateWorkOrderResponse xmlns="urn:aucklandcouncil.govt.nz:service:supplier:operations:componentmodel:order" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+		<updateResponse xmlns="">
+			<orderId>003101034422</orderId>
+			<supplierId>901630</supplierId>
+			<supplierOrderId>3307122</supplierOrderId>
+			<status>COMP</status>
+  		</updateResponse>
+	</updateWorkOrderResponse>
+  </soap:Body>
+</soap:Envelope>`);
+
   } else if (request.method === 'GET' && request.url === '/file'){
     response.writeHead(200, { 'Content-Type': 'text/plain', 'Content-disposition': 'attachment; filename=' + 'testPDF.pdf' });
 	filestream = fs.createReadStream('./testPDF.pdf');
+	filestream.pipe(response);
+  } else if (request.method === 'GET' && request.url === '/file_like_SAP'){
+    response.writeHead(200, { 'server': 'SAP NetWeaver Application Server 7.49 / AS Java 7.50', 'content-type': 'application/pdf;charset=;version=0046'}); //', 'content-length': '27319'});-->removed content length
+	filestream = fs.createReadStream('./testPDF2.pdf');
+	filestream.pipe(response);
+  } else if (request.method === 'GET' && request.url === '/file_modif'){
+    response.writeHead(200, { 'server': 'SAP NetWeaver Application Server 7.49 / AS Java 7.50','content-type': 'application/pdf;charset=UTF-8;version=1.0'});
+	filestream = fs.createReadStream('./testPDF2.pdf');
 	filestream.pipe(response);
   } else if (request.method === 'POST' && request.url === '/wsfileattachment'){
     response.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -49,7 +74,18 @@ http.createServer((request, response) => {
 	console.log("hey! i'm called axis2_postfile");
   } else {
     response.writeHead(404, { 'Content-Type': 'text/plain' });
-    response.end(`Hello, what the heck is ${request.url} ??? please explain.`);
+    response.end(`Hello, called using ${request.url} Doco: 
+			the following calls can be used:
+			POST /request
+			GET /file
+			GET /file_like_sap
+			GET /file_modif
+			POST /wsfileattachment
+			POST /axis2_postfile (when axis2 is the caller)		
+			
+			GET /echo
+	
+		`);
     console.error(`Hello, what the heck is ${request.url} ??? please explain.`);
   }
 }).listen(port);
---
+++ pfp_web/pfp2.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/PFP', express.static('//aklc.govt.nz/shared/CityWide/APPS/PIP/TEST/DRAFT/PFP/9999_PFP_TEST_CZ3'), (req,res,next) => {
+	console.log(`/PFP requested of type:${req.method}, ${req.originalUrl}`);
+	next();
+});
+
+app.listen(4040, () => console.log('listening to port 4040'));
+
